97ba8f19d284e5b5408b7961c10fa342ea3a088e
Kyle Kernick <kkernick@protonmail.com>
2025-03-13 23:42:45 -0600

    PKGBUILD changes

--- cpp/docs/html/search/all_2.js
+++ cpp/docs/html/search/all_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['cmd_0',['cmd',['../namespacegenerate.html#a38dcdfdf9293b8aafe77392021d65019',1,'generate']]],
-  ['command_20line_20reference_1',['Command Line Reference',['../md_docs_2md_2USAGE.html#autotoc_md8',1,'']]],
+  ['command_20line_20reference_1',['Command Line Reference',['../md_docs_2md_2USAGE.html#autotoc_md3',1,'']]],
   ['config_2',['config',['../structarg_1_1config.html',1,'arg::config'],['../namespacearg.html#aa4691d7d844d1ab0993f5af377522807',1,'arg::config']]],
   ['create_3',['create',['../classshared_1_1TemporaryDirectory.html#a26fcda7543faa65e33b216885004fe17',1,'shared::TemporaryDirectory']]]
 ];
